b8401e2dc4f4a184adb03fd75fe973f4f37e7a63
drgkikas <paschalis.gkikas@gmail.com>
2026-04-04 20:10:30 +0300

    robotTXT

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://drgkikas-astro.pages.dev',
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
