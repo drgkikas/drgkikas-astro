ea92b2057f8629f211ccc77616a1e433e613efeb
drgkikas <paschalis.gkikas@gmail.com>
2026-04-06 10:01:09 +0300

    fix: post-build creates dist/_worker.js for Cloudflare Pages SSR Worker detection

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   site: 'https://drgkikas.gr',
   adapter: cloudflare({
     imageService: 'compile',
