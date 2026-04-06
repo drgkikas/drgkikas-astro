d520d0922b3ee92dd5e452d9423875c33d3e4d0f
drgkikas <paschalis.gkikas@gmail.com>
2026-04-06 09:19:43 +0300

    test5

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ export default defineConfig({
   site: 'https://drgkikas.gr',
   adapter: cloudflare({
     imageService: 'compile',
-    sessionKVBindingName: undefined,
   }),
   vite: {
     plugins: [tailwindcss()]
