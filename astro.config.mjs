5ac9a9c492f01acd21f1f0d8b3cfb10d01955f6c
drgkikas <paschalis.gkikas@gmail.com>
2026-04-06 09:57:45 +0300

    xalia3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   site: 'https://drgkikas.gr',
   adapter: cloudflare({
     imageService: 'compile',
